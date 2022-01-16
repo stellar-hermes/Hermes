331fa7f3261b0e72b48f05fc0d5e1b8c1d3b1b2f
Kamyarz <kamibagha@gmail.com>
2022-01-16 14:10:03 +0330

    adding models

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# %%
 import nltk
 # nltk.download()
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -6,11 +7,25 @@ import numpy as np
 import random
 import string # to process standard python strings
 
+# %%
 
 f=open('chatbot.txt','r',errors = 'ignore')
 raw=f.read()
 raw=raw.lower()# converts to lowercase
-nltk.download('punkt') # first-time use only
+# nltk.download('punkt') # first-time use only
 # nltk.download('wordnet') # first-time use only
-# sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
-# word_tokens = nltk.word_tokenize(raw)# converts to list of words
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+print('everything is')
+# %%
+
+sent_tokens[:2]
+
+# %%
+lemmer = nltk.stem.WordNetLemmatizer()
+#WordNet is a semantically-oriented dictionary of English included in NLTK.
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
